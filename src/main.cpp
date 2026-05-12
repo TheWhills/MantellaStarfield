4e2a282c05707561e08b43c23a84dde82087e64a
1100340202 <tjanderson05@icloud.com>
2026-05-12 10:50:17 -0400

    Working dialogue detection and HTTP - blocked on MenuTopicManager ID

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #undef ERROR
 #include <string>
 #include <sstream>
+#include <thread>
+#include <chrono>
 
 #pragma comment(lib, "winhttp.lib")
 
@@ -123,66 +125,6 @@ std::string BuildStartConversationJSON(RE::TESNPC* npc, RE::TESObjectREFR* refr)
     return json.str();
 }
 
-class DialogueMenuListener : public RE::BSTEventSink<RE::MenuOpenCloseEvent>
-{
-public:
-    RE::BSEventNotifyControl ProcessEvent(
-        const RE::MenuOpenCloseEvent& a_event,
-        RE::BSTEventSource<RE::MenuOpenCloseEvent>*) override
-    {
-        if (a_event.menuName == "DialogueMenu" && a_event.opening) {
-            REX::INFO("Mantella: DialogueMenu opened");
-
-            auto* topicManager = RE::MenuTopicManager::GetSingleton();
-            if (!topicManager) {
-                REX::ERROR("Mantella: MenuTopicManager not found");
-                return RE::BSEventNotifyControl::kContinue;
-            }
-
-            auto speakerHandle = topicManager->speaker;
-            auto* speakerRef = speakerHandle.get().get();
-            if (!speakerRef) {
-                REX::ERROR("Mantella: Speaker reference is null");
-                return RE::BSEventNotifyControl::kContinue;
-            }
-
-            auto* npc = speakerRef->As<RE::TESNPC>();
-            if (!npc) {
-                auto basePtr = speakerRef->GetBaseObject();
-                auto* base = basePtr.get();
-                if (base) npc = base->As<RE::TESNPC>();
-            }
-
-            if (!npc) {
-                REX::ERROR("Mantella: Could not get TESNPC from speaker");
-                return RE::BSEventNotifyControl::kContinue;
-            }
-
-            REX::INFO("Mantella: Talking to {}", npc->GetFullName());
-
-            std::string json = BuildStartConversationJSON(npc, speakerRef);
-            if (!json.empty()) {
-                REX::INFO("Mantella: Sending JSON: {}", json);
-                SendToMantella(json);
-            }
-        }
-
-        if (a_event.menuName == "DialogueMenu" && !a_event.opening) {
-            REX::INFO("Mantella: DialogueMenu closed, ending conversation");
-            std::string json = "{\"mantella_request_type\":\"mantella_end_conversation\"}";
-            SendToMantella(json);
-        }
-
-        return RE::BSEventNotifyControl::kContinue;
-    }
-
-    static DialogueMenuListener* GetSingleton()
-    {
-        static DialogueMenuListener instance;
-        return &instance;
-    }
-};
-
 SFSE_PLUGIN_PRELOAD(const SFSE::PreLoadInterface* a_sfse)
 {
     SFSE::Init(a_sfse);
@@ -192,19 +134,57 @@ SFSE_PLUGIN_PRELOAD(const SFSE::PreLoadInterface* a_sfse)
 SFSE_PLUGIN_LOAD(const SFSE::LoadInterface* a_sfse)
 {
     SFSE::Init(a_sfse);
-
-    SFSE::GetMessagingInterface()->RegisterListener([](SFSE::MessagingInterface::Message* a_msg) {
-        if (a_msg->type == SFSE::MessagingInterface::kPostLoad) {
-            auto* ui = RE::UI::GetSingleton();
-            if (ui) {
-                ui->RegisterSink<RE::MenuOpenCloseEvent>(DialogueMenuListener::GetSingleton());
-                REX::INFO("Mantella: Registered DialogueMenu listener");
-            } else {
-                REX::ERROR("Mantella: Could not get UI singleton");
-            }
-        }
-    });
-
     REX::INFO("Mantella Starfield plugin loaded");
+
+    std::thread([]() {
+        std::this_thread::sleep_for(std::chrono::seconds(15));
+        REX::INFO("Mantella: Starting dialogue monitor thread");
+
+        bool wasDialogueOpen = false;
+
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+            auto* ui = RE::UI::GetSingleton();
+            if (!ui) continue;
+
+            bool isDialogueOpen = ui->IsMenuOpen("DialogueMenu");
+
+            if (isDialogueOpen && !wasDialogueOpen) {
+                REX::INFO("Mantella: DialogueMenu opened (polled)");
+
+                auto* topicManager = RE::MenuTopicManager::GetSingleton();
+                if (topicManager) {
+                    auto* speakerRef = topicManager->speaker.get().get();
+                    if (speakerRef) {
+                        auto* npc = speakerRef->As<RE::TESNPC>();
+                        if (!npc) {
+                            auto basePtr = speakerRef->GetBaseObject();
+                            auto* base = basePtr.get();
+                            if (base) npc = base->As<RE::TESNPC>();
+                        }
+                        if (npc) {
+                            REX::INFO("Mantella: Talking to {}", npc->GetFullName());
+                            std::string json = BuildStartConversationJSON(npc, speakerRef);
+                            SendToMantella(json);
+                        } else {
+                            REX::ERROR("Mantella: Could not get TESNPC from speaker");
+                        }
+                    } else {
+                        REX::ERROR("Mantella: Speaker reference is null");
+                    }
+                } else {
+                    REX::ERROR("Mantella: MenuTopicManager not found");
+                }
+
+            } else if (!isDialogueOpen && wasDialogueOpen) {
+                REX::INFO("Mantella: DialogueMenu closed (polled)");
+                SendToMantella("{\"mantella_request_type\":\"mantella_end_conversation\"}");
+            }
+
+            wasDialogueOpen = isDialogueOpen;
+        }
+    }).detach();
+
     return true;
 }
